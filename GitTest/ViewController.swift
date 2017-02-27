acb57e05a6222e897ef8d619cc21f4ea07965fdb
Antonio Esposito <antonioesposito@MBP-di-Antonio.lan>
2017-02-27 11:36:28 +0100

    Modifica codice

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+ 
+    @IBAction func click(_ sender: Any) {
+        
+        print("ciao dev")
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+ 
 
 }
 
